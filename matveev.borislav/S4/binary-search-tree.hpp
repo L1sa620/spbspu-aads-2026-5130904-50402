0951122a96eedd50d6310618dd339724c190499f
L1sa620 <lisevbom@gmail.com>
2026-05-18 20:26:05 +0300

    Implementation tree iterators

--- matveev.borislav/S4/binary-search-tree.hpp
+++ matveev.borislav/S4/binary-search-tree.hpp
@@ -26,8 +26,59 @@ private:
   };
 
 public:
-  class Iterator;
   class ConstIterator;
+
+  class Iterator
+  {
+    friend class BSTree< Key, Value, Compare >;
+    friend class ConstIterator;
+
+  public:
+    Iterator();
+
+    std::pair< Key, Value >& operator*() const;
+    std::pair< Key, Value >* operator->() const;
+
+    Iterator& operator++();
+    Iterator operator++(int);
+    Iterator& operator--();
+    Iterator operator--(int);
+
+    bool operator==(const Iterator& rhs) const noexcept;
+    bool operator!=(const Iterator& rhs) const noexcept;
+
+  private:
+    Iterator(Node* node, Node* fake);
+
+    Node* node_;
+    Node* fake_;
+  };
+
+  class ConstIterator
+  {
+    friend class BSTree< Key, Value, Compare >;
+
+  public:
+    ConstIterator();
+    ConstIterator(const Iterator& iter);
+
+    const std::pair< Key, Value >& operator*() const;
+    const std::pair< Key, Value >* operator->() const;
+
+    ConstIterator& operator++();
+    ConstIterator operator++(int);
+    ConstIterator& operator--();
+    ConstIterator operator--(int);
+
+    bool operator==(const ConstIterator& rhs) const noexcept;
+    bool operator!=(const ConstIterator& rhs) const noexcept;
+
+  private:
+    ConstIterator(Node* node, Node* fake);
+
+    Node* node_;
+    Node* fake_;
+  };
 
   BSTree();
   BSTree(const BSTree& other);
@@ -69,6 +120,8 @@ public:
 
 private:
   Node* findNode(const Key& key) const noexcept;
+  static Node* minNode(Node* node, Node* fake) noexcept;
+  static Node* maxNode(Node* node, Node* fake) noexcept;
   static void deleteSubtree(Node* node) noexcept;
 
   Node fake_;
@@ -91,6 +144,182 @@ BSTree< Key, Value, Compare >::Node::Node(const Key& key, const Value& value):
   left(nullptr),
   right(nullptr)
 {}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::Iterator::Iterator():
+  node_(nullptr),
+  fake_(nullptr)
+{}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::Iterator::Iterator(Node* node, Node* fake):
+  node_(node),
+  fake_(fake)
+{}
+
+template< class Key, class Value, class Compare >
+std::pair< Key, Value >& BSTree< Key, Value, Compare >::Iterator::operator*() const
+{
+  return node_->data;
+}
+
+template< class Key, class Value, class Compare >
+std::pair< Key, Value >* BSTree< Key, Value, Compare >::Iterator::operator->() const
+{
+  return &node_->data;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator& BSTree< Key, Value, Compare >::Iterator::operator++()
+{
+  if (node_->right != nullptr)
+  {
+    node_ = minNode(node_->right, fake_);
+    return *this;
+  }
+
+  Node* parent = node_->parent;
+
+  while (parent != nullptr && node_ == parent->right)
+  {
+    node_ = parent;
+    parent = parent->parent;
+  }
+
+  node_ = parent == nullptr ? fake_ : parent;
+  return *this;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator BSTree< Key, Value, Compare >::Iterator::operator++(int)
+{
+  Iterator result(*this);
+  ++(*this);
+  return result;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator& BSTree< Key, Value, Compare >::Iterator::operator--()
+{
+  if (node_ == fake_)
+  {
+    node_ = maxNode(fake_->left, fake_);
+    return *this;
+  }
+
+  if (node_->left != nullptr)
+  {
+    node_ = maxNode(node_->left, fake_);
+    return *this;
+  }
+
+  Node* parent = node_->parent;
+
+  while (parent != nullptr && node_ == parent->left)
+  {
+    node_ = parent;
+    parent = parent->parent;
+  }
+
+  node_ = parent == nullptr ? fake_ : parent;
+  return *this;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator BSTree< Key, Value, Compare >::Iterator::operator--(int)
+{
+  Iterator result(*this);
+  --(*this);
+  return result;
+}
+
+template< class Key, class Value, class Compare >
+bool BSTree< Key, Value, Compare >::Iterator::operator==(const Iterator& rhs) const noexcept
+{
+  return node_ == rhs.node_;
+}
+
+template< class Key, class Value, class Compare >
+bool BSTree< Key, Value, Compare >::Iterator::operator!=(const Iterator& rhs) const noexcept
+{
+  return !(*this == rhs);
+}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::ConstIterator::ConstIterator():
+  node_(nullptr),
+  fake_(nullptr)
+{}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::ConstIterator::ConstIterator(const Iterator& iter):
+  node_(iter.node_),
+  fake_(iter.fake_)
+{}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::ConstIterator::ConstIterator(Node* node, Node* fake):
+  node_(node),
+  fake_(fake)
+{}
+
+template< class Key, class Value, class Compare >
+const std::pair< Key, Value >& BSTree< Key, Value, Compare >::ConstIterator::operator*() const
+{
+  return node_->data;
+}
+
+template< class Key, class Value, class Compare >
+const std::pair< Key, Value >* BSTree< Key, Value, Compare >::ConstIterator::operator->() const
+{
+  return &node_->data;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator& BSTree< Key, Value, Compare >::ConstIterator::operator++()
+{
+  Iterator iter(node_, fake_);
+  ++iter;
+  node_ = iter.node_;
+  return *this;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::ConstIterator::operator++(int)
+{
+  ConstIterator result(*this);
+  ++(*this);
+  return result;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator& BSTree< Key, Value, Compare >::ConstIterator::operator--()
+{
+  Iterator iter(node_, fake_);
+  --iter;
+  node_ = iter.node_;
+  return *this;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::ConstIterator::operator--(int)
+{
+  ConstIterator result(*this);
+  --(*this);
+  return result;
+}
+
+template< class Key, class Value, class Compare >
+bool BSTree< Key, Value, Compare >::ConstIterator::operator==(const ConstIterator& rhs) const noexcept
+{
+  return node_ == rhs.node_;
+}
+
+template< class Key, class Value, class Compare >
+bool BSTree< Key, Value, Compare >::ConstIterator::operator!=(const ConstIterator& rhs) const noexcept
+{
+  return !(*this == rhs);
+}
 
 template< class Key, class Value, class Compare >
 BSTree< Key, Value, Compare >::BSTree():
@@ -227,6 +456,57 @@ const Value& BSTree< Key, Value, Compare >::at(const Key& key) const
 }
 
 template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator BSTree< Key, Value, Compare >::begin() noexcept
+{
+  return Iterator(minNode(fake_.left, &fake_), &fake_);
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator BSTree< Key, Value, Compare >::end() noexcept
+{
+  return Iterator(&fake_, &fake_);
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::begin() const noexcept
+{
+  return cbegin();
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::end() const noexcept
+{
+  return cend();
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::cbegin() const noexcept
+{
+  return ConstIterator(minNode(fake_.left, const_cast< Node* >(&fake_)), const_cast< Node* >(&fake_));
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::cend() const noexcept
+{
+  return ConstIterator(const_cast< Node* >(&fake_), const_cast< Node* >(&fake_));
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Iterator BSTree< Key, Value, Compare >::find(const Key& key) noexcept
+{
+  Node* node = findNode(key);
+  return Iterator(node == nullptr ? &fake_ : node, &fake_);
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::ConstIterator BSTree< Key, Value, Compare >::find(const Key& key) const noexcept
+{
+  Node* node = findNode(key);
+  Node* fake = const_cast< Node* >(&fake_);
+  return ConstIterator(node == nullptr ? fake : node, fake);
+}
+
+template< class Key, class Value, class Compare >
 void BSTree< Key, Value, Compare >::clear() noexcept
 {
   deleteSubtree(fake_.left);
@@ -256,6 +536,38 @@ typename BSTree< Key, Value, Compare >::Node* BSTree< Key, Value, Compare >::fin
   }
 
   return nullptr;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Node* BSTree< Key, Value, Compare >::minNode(Node* node, Node* fake) noexcept
+{
+  if (node == nullptr)
+  {
+    return fake;
+  }
+
+  while (node->left != nullptr)
+  {
+    node = node->left;
+  }
+
+  return node;
+}
+
+template< class Key, class Value, class Compare >
+typename BSTree< Key, Value, Compare >::Node* BSTree< Key, Value, Compare >::maxNode(Node* node, Node* fake) noexcept
+{
+  if (node == nullptr)
+  {
+    return fake;
+  }
+
+  while (node->right != nullptr)
+  {
+    node = node->right;
+  }
+
+  return node;
 }
 
 template< class Key, class Value, class Compare >
--- matveev.borislav/S4/test-tree.cpp
+++ matveev.borislav/S4/test-tree.cpp
@@ -12,6 +12,7 @@ BOOST_AUTO_TEST_CASE(empty_tree)
   BOOST_TEST(tree.empty());
   BOOST_TEST(tree.size() == 0);
   BOOST_TEST(!tree.has(1));
+  BOOST_TEST(static_cast< bool >(tree.begin() == tree.end()));
 }
 
 BOOST_AUTO_TEST_CASE(push_has_and_at)
@@ -51,6 +52,55 @@ BOOST_AUTO_TEST_CASE(at_missing_key)
   BOOST_CHECK_THROW(tree.at(1), std::logic_error);
 }
 
+BOOST_AUTO_TEST_CASE(iteration_is_sorted)
+{
+  matveev::BSTree< int, std::string > tree;
+
+  tree.push(3, "three");
+  tree.push(1, "one");
+  tree.push(2, "two");
+
+  matveev::BSTree< int, std::string >::Iterator it = tree.begin();
+
+  BOOST_TEST(it->first == 1);
+  ++it;
+  BOOST_TEST(it->first == 2);
+  ++it;
+  BOOST_TEST(it->first == 3);
+  ++it;
+  BOOST_TEST(static_cast< bool >(it == tree.end()));
+}
+
+BOOST_AUTO_TEST_CASE(reverse_iteration)
+{
+  matveev::BSTree< int, int > tree;
+
+  tree.push(2, 20);
+  tree.push(1, 10);
+  tree.push(3, 30);
+
+  matveev::BSTree< int, int >::Iterator it = tree.end();
+
+  --it;
+  BOOST_TEST(it->first == 3);
+  --it;
+  BOOST_TEST(it->first == 2);
+  --it;
+  BOOST_TEST(it->first == 1);
+}
+
+BOOST_AUTO_TEST_CASE(find_existing_and_missing)
+{
+  matveev::BSTree< int, int > tree;
+
+  tree.push(2, 20);
+  tree.push(1, 10);
+  tree.push(3, 30);
+
+  BOOST_TEST(tree.find(2)->second == 20);
+  BOOST_TEST(static_cast< bool >(tree.find(4) == tree.end()));
+}
+
 BOOST_AUTO_TEST_CASE(clear_tree)
 {
   matveev::BSTree< int, int > tree;
@@ -64,4 +114,5 @@ BOOST_AUTO_TEST_CASE(clear_tree)
   BOOST_TEST(tree.empty());
   BOOST_TEST(tree.size() == 0);
   BOOST_TEST(!tree.has(2));
+  BOOST_TEST(static_cast< bool >(tree.begin() == tree.end()));
 }
