1108cd4dfd7c73ae4fc11fcd0eee4abe2ac2064f
L1sa620 <lisevbom@gmail.com>
2026-05-22 00:37:21 +0300

    Add rebuild tests

--- matveev.borislav/S3/test-hash.cpp
+++ matveev.borislav/S3/test-hash.cpp
@@ -111,6 +111,10 @@ BOOST_AUTO_TEST_CASE(overflow_bucket_limit_throws)
 {
   matveev::HashTable< int, std::string, SameHash, IntEqual > table(1, 2);
 
+  table.maxLoadFactor(100.0);
+  table.maxSpareBucketSize(100);
+  table.maxAverageBucketSize(100.0);
+
   table.add(1, "one");
   table.add(2, "two");
   table.add(3, "three");
@@ -191,4 +195,59 @@ BOOST_AUTO_TEST_CASE(iterator_visits_only_occupied_items)
   }
 
   BOOST_TEST(count == 3);
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_statistics)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(2, 2);
+
+  table.maxLoadFactor(100.0);
+  table.maxSpareBucketSize(100);
+  table.maxAverageBucketSize(100.0);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  BOOST_TEST(table.size() == 3);
+  BOOST_TEST(table.bucketCount() == 2);
+  BOOST_TEST(table.bucketCapacity() == 2);
+  BOOST_TEST(table.loadFactor() == 0.75);
+  BOOST_TEST(table.averageBucketSize() == 1.5);
+  BOOST_TEST(table.maxBucketSize() == 2);
+  BOOST_TEST(table.spareBucketSize() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(auto_rehash_by_load_factor)
+{
+  matveev::HashTable< int, std::string, IntHash, IntEqual > table(2, 2);
+
+  table.maxLoadFactor(0.5);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  BOOST_TEST(table.bucketCount() > 2);
+  BOOST_TEST(table.has(1));
+  BOOST_TEST(table.has(2));
+  BOOST_TEST(table.has(3));
+}
+
+BOOST_AUTO_TEST_CASE(auto_rehash_by_spare_bucket_size)
+{
+  matveev::HashTable< int, std::string, SameHash, IntEqual > table(2, 2);
+
+  table.maxLoadFactor(100.0);
+  table.maxAverageBucketSize(100.0);
+  table.maxSpareBucketSize(0);
+
+  table.add(1, "one");
+  table.add(2, "two");
+  table.add(3, "three");
+
+  BOOST_TEST(table.bucketCapacity() > 2);
+  BOOST_TEST(table.has(1));
+  BOOST_TEST(table.has(2));
+  BOOST_TEST(table.has(3));
 }
