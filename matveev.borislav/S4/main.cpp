ae7403d312e520d97a6bca636e3008f131d71f31
L1sa620 <lisevbom@gmail.com>
2026-05-22 21:18:21 +0300

    Fix main

--- matveev.borislav/S4/main.cpp
+++ matveev.borislav/S4/main.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include "dictionary-commands.hpp"
+#include "dictionary-reader.hpp"
 
-int main()
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << matveev::INVALID_COMMAND << '\n';
+    return 1;
+  }
+
+  std::ifstream input(argv[1]);
+
+  if (!input)
+  {
+    std::cerr << matveev::INVALID_COMMAND << '\n';
+    return 1;
+  }
+
+  matveev::DictionaryStorage storage;
+
+  try
+  {
+    storage = matveev::readDictionaries(input);
+  }
+  catch (const std::exception&)
+  {
+    std::cerr << matveev::INVALID_COMMAND << '\n';
+    return 1;
+  }
+
+  std::string line;
+
+  while (std::getline(std::cin, line))
+  {
+    matveev::executeCommand(std::cout, storage, line);
+  }
+
   return 0;
 }
